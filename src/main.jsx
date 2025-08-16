f85ea3bc5a09922b7a17f948260b528173cfc36f
mridul45 <mridulsinghal474@gmail.com>
2025-08-16 19:15:10 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import store from './store/store';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Ahara/">
         <App />
       </BrowserRouter>
     </Provider>
